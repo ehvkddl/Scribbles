6a6fab8f7926090f12cfb629a5ef1a60d7ffac6c
ehvkddl <ehvkddl@naver.com>
2022-02-09 23:59:28 +0900

    Practice

--- Init.playground/Contents.swift
+++ Init.playground/Contents.swift
@@ -1,3 +1,25 @@
-import UIKit
+struct Resolution {
+    var width = 0
+    var height = 0
+    
+    init(width: Int) {
+        self.width = width
+    }
+}
 
-var greeting = "Hello, playground"
+class VideoMode {
+    var resolution = Resolution(width: 2048)
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+    
+    init(interlaced: Bool, frameRate: Double) {
+        self.interlaced = interlaced
+        self.frameRate
+    }
+}
+
+let reslution = Resolution(width: 4096)
+let videoMode = VideoMode(interlaced: true, frameRate: 40.0)
+
+
